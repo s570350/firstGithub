2b89fd91acf442891b0db5741ad3aa2ba79fd377
s570350 <s570350@nwmissouri.edu>
2024-01-24 20:34:35 -0600

    First Commit

---
+++ JavaApplication4/src/javaapplication4/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication4;
+
+/**
+ *
+ * @author S570350
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+    
+}
