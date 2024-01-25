e098a67dd35cfb39376ac8cfe4da37bfcadbdbc8
s570350 <s570350@nwmissouri.edu>
2024-01-24 20:43:04 -0600

    Second commit

---
+++ JavaApplication4/src/javaapplication4/SecondGitCommit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication4;
+
+/**
+ *
+ * @author S570350 Sargari Nikilesh Reddy
+ */
+public class SecondGitCommit {
+    public static void main(String args[]){
+        System.out.println("Hello World.I am coming for you");
+    }
+    
+}
